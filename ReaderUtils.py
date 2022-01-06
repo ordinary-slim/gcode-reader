bc288d7d5bc693dbc41fa339f4adf4356dd836bf
ordinary-slim <mehdi.slimani.elmaddarsi@gmail.com>
2022-01-06 17:29:17 +0100

    MVP: Fixed bugs, added .vtk export, changed data structs.

--- ReaderUtils.py
+++ ReaderUtils.py
@@ -1,5 +1,6 @@
 import re
 import pdb
+import logging
 
 #Utilities to read Gcode lines into Python data structures.
 #gcode lines are stored into dictionnaries with each key corresponding
@@ -48,105 +49,152 @@ def readGcodeLine(line: str):
 
     return output
 
+def hasExtrusion( gcodeLine: dict ):
+    '''
+    Determines if the gcode line describes extrusion.
+    '''
+    if 'E' in gcodeLine and 'X' in gcodeLine:
+        return True
+    else:
+        return False
+
+def getPoint( gcodeLine: dict, currPoint: tuple ):
+    '''
+    Determines if the gcode line describes a new point.
+    '''
+    x, y, z = currPoint
+    hasPoint = False
+    if 'X' in gcodeLine:
+        #update X
+        x = gcodeLine['X']
+        hasPoint = True
+    if 'Y' in gcodeLine:
+        #update Y
+        y = gcodeLine['Y']
+        hasPoint = True
+    if 'Z' in gcodeLine:
+        #update Z:
+        z = gcodeLine['Z']
+        hasPoint = True
+    if hasPoint:
+        return (x, y, z)
+    else:
+        return ()
+
 def readGcodeFile(File: str):
     '''
     Read Gcode file and stores lines with extrusion.
 
     File is a string with the path to the gcode file.
-    output will be a list of entries with 2 entries in R3,
+    segmentList will be a list of entries with 2 entries in R3,
     corresponding to the lines with extrusion in File.
     '''
     #initialize two empty dictionnaries for
     #the previous line and the current line
-    prevLine = {}
     currLine = {}
-    #initialize output to empty list
-    output = []
+    #initialize segmentList to empty list
+    pointList = []
+    connectivity = []
     with open(File, 'r') as FileHandle:
         lines = FileHandle.readlines()
 
-        #different treatment for the first line
-        currLine = readGcodeLine( lines[0] )
-        #this algorithm needs all lines to have a Z coordinate
-        if 'Z' not in currLine:
-            currLine['Z'] = 0.0
+        prevPoint = (0.0, 0.0, 0.0)
+        currPoint = (0.0, 0.0, 0.0)
 
-        #read remaining lines
-        for idx, line in enumerate( lines[1:] ):
-            print("About to process the following lines:")
-            print(lines[idx-1])
-            print(line)
-            prevLine = currLine
+        #read lines
+        for line in lines:
             currLine = readGcodeLine( line )
-            #if line has extrusion, store the associated segment
-            if 'E' in currLine:
-                if 'Z' not in currLine:
-                    currLine['Z'] = prevLine['Z']
 
-                segment = Segment( prevLine, currLine)
-                print(segment.write2string())
-                output.append(segment)
+            #contains a point?
+            newPoint = getPoint(currLine, currPoint)
+            if newPoint:
+                prevPoint = currPoint
+                currPoint = newPoint
 
-        return output
+            #if line has extrusion, store the associated segment and points
+            if hasExtrusion(currLine):
+                pointList.append( prevPoint )
+                pointList.append( currPoint )
+                #zero indexing
+                connectivity.append( (len(pointList)-2, len(pointList)-1) )
 
-class Segment:
+        return pointList, connectivity
+
+def write2TxtFile( file:str,
+        pointList: list[tuple], connectivity: list[tuple]):
     '''
-    3D line with origin (point1) and end (point2).
-    '''
-    point1 = (0.0, 0.0, 0.0)
-    point2 = (1.0, 0.0, 0.0)
-
-    def __init__(self, prevLine: dict, currLine: dict):
-        '''
-        Return a segment from two gcode lines.
-
-        prevLine and currLine must have X, Y and Z keys.
-        '''
-        self.point1 = ( prevLine['X'], prevLine['Y'], prevLine['Z'] )
-        self.point2 = ( currLine['X'], currLine['Y'], currLine['Z'] )
-
-    def write2string(self):
-        '''
-        Writes a segment to string.
-        
-        Resulting string: x1, y1, z1, x2, y2, z2
-        '''
-        #special treatment for first writing task
-        output = str( self.point1[0] )
-        #write rest
-        for coord in self.point1[1:] :
-            output = output + ", " + str( coord )
-        for coord in self.point2:
-            output = output + ", " + str( coord )
-
-        output += "\n"
-        return output
-
-def write2TxtFile( file:str, segmentList: list[Segment] ):
-    '''
-    Write list of segments to file.
+    Write points and connectivities to .txt
     '''
     with open(file, 'w') as f:
-        #write header
-        header = "X1, Y1, Z1, X2, Y2, Z2\n"
-        f.write(header)
-        #write segments
-        for seg in segmentList:
-            f.write( seg.write2string() )
+        #write points
+        ##points header
+        pointsHeader = "POINTS " + str(len(pointList)) + "\n"
+        f.write(pointsHeader)
+        ##points
+        for p in pointList:
+            #write the tuple without its parentheses
+            f.write( str(p)[1:-1]  + "\n")
 
+        #write connectivities
+        ##header
+        linesHeader = "LINES " + str(len(connectivity)) + "\n"
+        f.write(linesHeader)
+        ##lines
+        for l in connectivity:
+            #write the tuple without its parentheses
+            f.write( str(l)[1:-1] + "\n" )
+    return
+
+def write2VtkFile( file:str,
+        pointList: list[tuple], connectivity: list[tuple]):
+    '''
+    Write points and connectivities to .txt
+    '''
+    numPoints = len(pointList)
+    numLines = len(connectivity)
+    with open(file, 'w') as f:
+        #write header
+        f.write("# vtk DataFile Version 2.0\n")
+        #write title
+        f.write("Some gcode\n")
+        #write data type
+        f.write("ASCII\n")
+        #write geometry
+        f.write("DATASET POLYDATA\n")
+        ##points
+        f.write("POINTS " + str(numPoints) + " float\n")
+        for p in pointList:
+            #write the tuple without its parentheses and commas
+            tmp = re.sub( r"[,()]", "", str(p))
+            f.write( tmp + "\n" )
+        ##lines
+        f.write("LINES " + str(numLines) + " " + str(3*numLines) + "\n")
+        for l in connectivity:
+            #write the tuple without its parentheses and commas
+            tmp = "2 " + re.sub( r"[,()]", "", str(l))
+            f.write( tmp + "\n" )
     return
 
 if __name__=="__main__":
     lines = ["G1 X4.4 Y-4.4 Z0.3 E0.33107 asdasdasd",\
         "G00",\
         "X4.4 Y-4.4 Z0.3 E0.33107 asdasdasd"]
-    file = "gcodes/annotations_disp.gcode"
+    file = "gcodes/CFFFP_rodamiento.gcode"
 
     print("Trying out line reader...")
     for line in lines:
         output = readGcodeLine(line)
 
     print("Trying out file reader...")
-    segList = readGcodeFile( file )
-    print("Trying out file writer...")
-    write2TxtFile( "out.txt", segList )
+    print("Target:", file)
+    p, c = readGcodeFile( file )
+
+    print("Trying out .txt writer...")
+    txt = "out.txt"
+    write2TxtFile( txt, p, c )
+    print("Wrote to " + txt + "." )
+
+    print("Trying out .vtk writer...")
+    vtk = "out.vtk"
+    write2VtkFile( vtk, p, c )
+    print("Wrote to " + vtk + "." )
